d522051240eb5f0c3d86a52332bba2b9c4006135
fanpengfei <fanpengfei@jd.com>
2018-10-10 03:18:20 -0400

    update for base line.

---
+++ hpslib.c
@@ -0,0 +1,19 @@
+/**
+ * @file: hpslib.h
+ * @desc:
+ *
+ * Pengfei.Fan,  2018/10/10
+ *
+ * Copyright (c) 2018, jd.com.
+ * Copyright (c) 2018, jdcloud.com.
+ * All rights reserved.
+ **/
+
+#ifndef __HPSLIB_H__
+#define __HPSLIB_H__
+
+int libtest();
+
+
+#endif
+
